d2de72457a348fc69357efb6c19fe94266515a67
franly2 <franlybudipramana588@gmail.com>
2026-06-23 22:12:15 +0700

    deploy backend to railway

--- backup src/_vrp/dto/route.dto.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prettier/prettier */
-export class OptimizeRouteDto {
-  courierLat: number;
-  courierLng: number;
-  // Kedepannya  mungkin tambah array of coordinates pelanggan di sini
-}
--- backup src/sales/sales.controller.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common';
-import { SalesService } from './sales.service';
-
-@Controller('sales')
-export class SalesController {
-  constructor(private readonly salesService: SalesService) {}
-}
